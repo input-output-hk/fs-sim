32c0909a4b098f8f6bdd2888857860ed2d76d86c
Joris Dral <joris@well-typed.com>
2024-03-13 16:42:04 +0100

    Simulation and QSM tests for `HasBufFS`

--- fs-sim/src/System/FS/Sim/Pure.hs
+++ fs-sim/src/System/FS/Sim/Pure.hs
@@ -1,13 +1,24 @@
 {-# LANGUAGE BangPatterns               #-}
+{-# LANGUAGE DeriveFunctor              #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs               #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeFamilies               #-}
+
 module System.FS.Sim.Pure (
     PureSimFS
     -- opaque
   , pureHasFS
   , runPureSimFS
+    -- * HasBufFS
+  , pureHasBufFS
   ) where
 
 import           Control.Monad.Except
+import           Control.Monad.Primitive
+import           Control.Monad.ST
 import           Control.Monad.State
 
 import           System.FS.API
@@ -16,13 +27,42 @@ import qualified System.FS.Sim.MockFS as Mock
 import           System.FS.Sim.MockFS (MockFS)
 
 -- | Monad useful for running 'HasFS' in pure code
-newtype PureSimFS a = PureSimFS (StateT MockFS (Except FsError) a)
-  deriving (Functor, Applicative, Monad, MonadState MockFS, MonadError FsError)
+newtype PureSimFS s a = PureSimFS (StateT MockFS (ExceptT FsError (ST s)) a)
+  deriving (Functor)
 
-runPureSimFS :: PureSimFS a -> MockFS -> Either FsError (a, MockFS)
-runPureSimFS (PureSimFS act) !st = runExcept $ runStateT act st
+unPureSimFS :: PureSimFS s a -> StateT MockFS (ExceptT FsError (ST s)) a
+unPureSimFS (PureSimFS act) = act
 
-pureHasFS :: HasFS PureSimFS Mock.HandleMock
+instance Applicative (PureSimFS s) where
+  pure :: a -> PureSimFS s a
+  pure x = PureSimFS $ pure x
+
+  (<*>) :: PureSimFS s (a -> b) -> PureSimFS s a -> PureSimFS s b
+  f <*> x = PureSimFS (unPureSimFS f <*> unPureSimFS x)
+
+instance Monad (PureSimFS s) where
+  (>>=) :: PureSimFS s a -> (a -> PureSimFS s b) -> PureSimFS s b
+  x >>= f = PureSimFS $ unPureSimFS x >>= unPureSimFS . f
+
+instance MonadState MockFS (PureSimFS s) where
+  state :: (MockFS -> (a, MockFS)) -> PureSimFS s a
+  state = PureSimFS . state
+
+instance MonadError FsError (PureSimFS s) where
+  throwError :: FsError -> PureSimFS s a
+  throwError = PureSimFS . throwError
+
+  catchError :: PureSimFS s a -> (FsError -> PureSimFS s a) -> PureSimFS s a
+  catchError act catch = PureSimFS $ catchError (unPureSimFS act) (unPureSimFS . catch)
+
+instance PrimMonad (PureSimFS s) where
+  type PrimState (PureSimFS s) = s
+  primitive act = PureSimFS $ primitive act
+
+runPureSimFS :: (forall s. PureSimFS s a) -> MockFS -> Either FsError (a, MockFS)
+runPureSimFS act !st = runST $ runExceptT $ flip runStateT st $ unPureSimFS act
+
+pureHasFS :: HasFS (PureSimFS s) Mock.HandleMock
 pureHasFS = HasFS {
       dumpState                = Mock.dumpState
     , hOpen                    = Mock.hOpen
@@ -45,3 +85,12 @@ pureHasFS = HasFS {
     , mkFsErrorPath            = fsToFsErrorPathUnmounted
     , unsafeToFilePath         = \_ -> error "pureHasFS:unsafeToFilePath"
     }
+
+pureHasBufFS :: HasBufFS (PureSimFS s) Mock.HandleMock
+pureHasBufFS = HasBufFS {
+      hGetBufSome   = Mock.hGetBufSome
+    , hGetBufSomeAt = Mock.hGetBufSomeAt
+    , hPutBufSome   = Mock.hPutBufSome
+    , hPutBufSomeAt = Mock.hPutBufSomeAt
+    }
+
